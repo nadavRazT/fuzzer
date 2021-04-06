98babf09c1591fd3240880004d5963315a6e1fd9
nadav <nadav.raz.41@gmail.com>
2021-04-07 02:19:25 +0300

    hi

--- execution.py
+++ execution.py
@@ -1,18 +1,58 @@
+import argparse
 import sys
-import pdb
-import os
+import signal
 import subprocess
-import ptrace.debugger
+import ptrace.debugger as debugger
+import ptrace
 
-def execute_fuzz():
-    args = [sys.executable, '-c', 'import time; time.sleep(60)']
-    child_popen = subprocess.Popen(args)
-    pid = child_popen.pid
-    debugger = ptrace.debugger.PtraceDebugger()
-    process = debugger.addProcess(pid, False)
-    # process is a PtraceProcess instance
-    print("IP before: %#x" % process.getInstrPointer())
+NUM_OF_RUNS = 10
 
+config = {
+    "file": "data/test.jpg",
+    "target": ""
+}
+
+
+def execute_fuzz(dbg, data, i):
+    cmd = [config["target"], config["file"]]
+    pid = debugger.child.createChild(cmd, no_stdout=True, env=None)
+    process = dbg.addProcess(pid, True)
+    process.cont()
+    try:
+        sig = process.waitSignals()
+    except:
+        return
+
+    if sig.signum == signal.SIGSEGV:
+        process.detach()
+        with open("crashes/crash.{}.jpg".format(i), "wb+") as fh:
+            fh.write(data)
+
+def fuzz():
+    dbg = ptrace.debugger.PtraceDebugger()
+
+    i = 0
+    while NUM_OF_RUNS > i:
+        i += 1
+        data = mutate()
+        execute_fuzz(dbg, data,i)
+
+
+def update_config(args):
+    config["target"] = args.target
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", help="target program",
+                        required=True)
+    args = parser.parse_args()
+    update_config(args)
+    fuzz()
+
+
+def mutate():
+    return None
 
 if __name__ == "__main__":
-    execute_fuzz()
+    main()
